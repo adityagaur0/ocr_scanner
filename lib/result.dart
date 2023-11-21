f87eb80d51a450f4aa644acd0cc2c5b4c4241821
adityagaur0 <adityakumargaur0@gmail.com>
2023-11-21 14:02:01 +0000

    21 Nov 2023, 19:32

--- lib/result.dart
+++ lib/result.dart
@@ -4,6 +4,17 @@ class ResultScreen extends StatelessWidget {
   final String text;
 
   const ResultScreen({super.key, required this.text});
+  @override
+  void initState() {
+    // super.initState();
+    show(text);
+  }
+
+  show(text) {
+    if (text.length() == 3) {
+      print(text);
+    }
+  }
 
   @override
   Widget build(BuildContext context) => Scaffold(
