ab5f04436a81470fd1cec792632b340174450587
adityagaur0 <adityakumargaur0@gmail.com>
2023-11-21 14:10:34 +0000

    21 Nov 2023, 19:40

--- lib/result.dart
+++ lib/result.dart
@@ -5,16 +5,9 @@ class ResultScreen extends StatelessWidget {
 
   const ResultScreen({Key? key, required this.text}) : super(key: key);
 
-  void showText() {
-    if (text.length == 3) {
-      print("Currency is: $text");
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     // Call the method to show text based on the condition
-    showText();
 
     return Scaffold(
       appBar: AppBar(
@@ -22,14 +15,7 @@ class ResultScreen extends StatelessWidget {
       ),
       body: Container(
         padding: const EdgeInsets.all(30.0),
-        child: Text(
-          text,
-          style: TextStyle(
-            fontSize: text.length == 3
-                ? 40
-                : 16.0, // Adjust font size based on length
-          ),
-        ),
+        child: text.length == 3 ? Text(text) : Container(),
       ),
     );
   }
