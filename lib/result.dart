cba64ce13e74f2eb6aace5f9e0653d32cab3620f
adityagaur0 <adityakumargaur0@gmail.com>
2023-11-21 14:05:57 +0000

    21 Nov 2023, 19:35

--- lib/result.dart
+++ lib/result.dart
@@ -3,27 +3,34 @@ import 'package:flutter/material.dart';
 class ResultScreen extends StatelessWidget {
   final String text;
 
-  const ResultScreen({super.key, required this.text});
-  @override
-  void initState() {
-    // super.initState();
-    show(text);
-  }
+  const ResultScreen({Key? key, required this.text}) : super(key: key);
 
-  show(text) {
-    if (text.length() == 3) {
-      print(text);
+  void showText() {
+    if (text.length == 3) {
+      print("Currency is: $text");
     }
   }
 
   @override
-  Widget build(BuildContext context) => Scaffold(
-        appBar: AppBar(
-          title: const Text('Result'),
+  Widget build(BuildContext context) {
+    // Call the method to show text based on the condition
+    showText();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Result'),
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(30.0),
+        child: Text(
+          text,
+          style: TextStyle(
+            fontSize: text.length == 3
+                ? 24.0
+                : 16.0, // Adjust font size based on length
+          ),
         ),
-        body: Container(
-          padding: const EdgeInsets.all(30.0),
-          child: Text(text),
-        ),
-      );
+      ),
+    );
+  }
 }
